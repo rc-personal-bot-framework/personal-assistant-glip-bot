08cc9916d84b461d57a01e98e8efb4e8e95ae8f5
ZHAO Xudong <zxdong@gmail.com>
2019-08-06 15:44:23 +0800

    Edit readme

--- src/server/index.js
+++ src/server/index.js
@@ -19,14 +19,25 @@ export const onPostAdd = async ({
   if (handled) {
     return false
   }
-  let sign = shouldUseSignature
-    ? `\n(send by [${exports.name}](${exports.homepage}))`
-    : ''
-  console.log(text, textFiltered, group, user, handled, shouldUseSignature)
-  if (textFiltered.toLowerCase() === 'hello') {
-    await user.sendMessage(group.id, {
-      text: 'Hi' + sign
+  function wait (time) {
+    return new Promise(resolve => {
+      setTimeout(resolve, time)
     })
+  }
+  console.log(text, textFiltered, group, user, handled, shouldUseSignature)
+  if (textFiltered.toLowerCase() === '__api_test__') {
+    // do api test
+    for (let i = 0; i < 10; i++) {
+      // glip chats
+      await user.rc.get('/restapi/v1.0/glip/chats').catch(console.log)
+      // await user.rc.get('/restapi/v1.0/account/~/extension/~/address-book/contact')
+      await user.rc.get('/restapi/v1.0/account/~/extension/~').catch(console.log)
+      await user.rc.get('/restapi/v1.0/account/~/extension/~/message-store').catch(console.log)
+      await user.rc.get('/restapi/v1.0/account/~/extension/~/presence').catch(console.log)
+      await user.rc.get('/restapi/v1.0/glip/webhooks').catch(console.log)
+      await wait(60 * 1000)
+      console.log('done:', i)
+    }
   }
 }
 */
